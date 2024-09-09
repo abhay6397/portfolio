3fb3f896b1afd7a7a206939bac112d2efe08b4d1
abhay6397 <abha.fwd@gmail.com>
2024-09-09 14:37:44 +0530

    from modified

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,20 +13,28 @@ const Contact = () => {
         
 
         <form action="https://getform.io/f/byvpdvea" method='POST' className='flex flex-col w-full items-center'>
-            <input className='bg-gray-800 text-white
+            <input
+             required
+             className='bg-gray-800 text-white
              border-white border-2 rounded md:w-1/2 w-[75%] py-1 px-2 outline-none mb-4' 
              type="text" placeholder='Enter your name'
              name='name'/>
-            <input className='bg-gray-800 text-white 
+
+            <input
+            required
+            className='bg-gray-800 text-white 
              border-white border-2 rounded  md:w-1/2 w-[75%] py-1 px-2 outline-none mb-4' 
              type="text" placeholder='Enter your email address'
              name='email'/>
+
             <input className='bg-gray-800 text-white 
              border-white border-2 rounded md:w-1/2 w-[75%] py-1 px-2 pb-44 outline-none mb-4' 
               type="text" placeholder='Enter your message' 
               name='message'/>
+
             <button
-             className=' bg-gradient-to-r from-cyan-500 to to-blue-500 cursor-pointer font-semibold py-2 px-3 rounded-md'
+             className=' bg-gradient-to-r from-cyan-500 to to-blue-500 cursor-pointer
+              font-semibold py-2 px-3 rounded-md'
              >Let's talk</button>
         </form>
         </div>
